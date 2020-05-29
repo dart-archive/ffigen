23c5be3a1c58f7a9c2b50f3517f7ede36579d699
Prerak Mann <mannprerak2@gmail.com>
2020-05-30 00:49:17 +0530

    tests: added native_function_test

---
+++ test/native_functions_test/buildDylib.sh
@@ -0,0 +1,1 @@
+clang -shared -fpic native_functions.c -o native_functions.so
---
+++ test/native_functions_test/native_functions.c
@@ -0,0 +1,21 @@
+#include <stdint.h>
+
+uint8_t Function1Uint8(uint8_t x) { return x + 42; }
+
+uint16_t Function1Uint16(uint16_t x) { return x + 42; }
+
+uint32_t Function1Uint32(uint32_t x) { return x + 42; }
+
+uint64_t Function1Uint64(uint64_t x) { return x + 42; }
+
+int8_t Function1Int8(int8_t x) { return x + 42; }
+
+int16_t Function1Int16(int16_t x) { return x + 42; }
+
+int32_t Function1Int32(int32_t x) { return x + 42; }
+
+int64_t Function1Int64(int64_t x) { return x + 42; }
+
+float Function1Float(float x) { return x + 42.0f; }
+
+double Function1Double(double x) { return x + 42.0; }
