e712abcfd8a78aec77018a218b21616b393be7f7
Prerak Mann <mannprerak2@gmail.com>
2020-07-23 14:15:58 +0530

    remove unused import

--- lib/src/header_parser/parser.dart
+++ lib/src/header_parser/parser.dart
@@ -3,7 +3,6 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'dart:ffi';
-import 'dart:io';
 
 import 'package:ffi/ffi.dart';
 import 'package:ffigen/src/code_generator.dart';
