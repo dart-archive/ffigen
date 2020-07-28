7445c8d5cfe172a62b3c1bcfdf069632d02a5088
Prerak Mann <mannprerak2@gmail.com>
2020-07-28 16:45:32 +0530

    updated macro test4 comment

--- test/header_parser_tests/macros.h
+++ test/header_parser_tests/macros.h
@@ -3,14 +3,14 @@
 #define TEST3 (TEST1 + TEST2)
 #define TEST4 "test"
 
-// The macro variable for this generated wouldn't compile, but libclang assigns
-// it a value of 4.
+// The comma operator should actually return the last value (3),
+// when returned by a function. This value, however, when assigned to a variable
+// will not compile and so libclang tries to fix it and assigns the first
+// value (4) to the generated macro variable.
 #define TEST5 4, \
               2, \
               3
 #define TEST6 (1 == 1);
 #define TEST7(x, y) x *y
 
-// The macro variable for this generated wouldn't compile, but libclang assigns
-// it a value of 5.
 #define TEST8 5,2,3
