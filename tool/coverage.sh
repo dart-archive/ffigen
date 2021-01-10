afba7533f42a6cdc32f4cddfc916b8f2be48f320
Prerak Mann <mannprerak2@gmail.com>
2021-01-10 14:52:15 +0530

    Use flag when running tests on travis

--- tool/coverage.sh
+++ tool/coverage.sh
@@ -11,7 +11,7 @@ set -e
 pub global activate remove_from_coverage
 pub global activate coverage
 # Generate coverage report.
-dart --pause-isolates-on-exit --disable-service-auth-codes --enable-vm-service=3000 test/test_coverage.dart &
+dart --no-sound-null-safety  --pause-isolates-on-exit --disable-service-auth-codes --enable-vm-service=3000 test/test_coverage.dart &
 dart pub global run coverage:collect_coverage --wait-paused --uri=http://127.0.0.1:3000/ -o coverage.json --resume-isolates
 dart pub global run coverage:format_coverage --lcov -i coverage.json -o lcov.info
 
