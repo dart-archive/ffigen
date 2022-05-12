fd692fac75762b7f046ef7df261cfc0b275d6379
Liam Appelbe <liama@google.com>
2022-05-12 13:51:06 -0700

    Finish comment

--- test/native_objc_test/util.dart
+++ test/native_objc_test/util.dart
@@ -13,6 +13,7 @@ void generateBindingsForCoverage(String testName) {
   // The ObjC test bindings are generated in setup.dart (see #362), which means
   // that the ObjC related bits of ffigen are missed by test coverage. So this
   // function just regenerates those bindings. It doesn't test anything except
+  // that the generation succeeded, by asserting the file exists.
   final config = Config.fromYaml(loadYaml(
       File(path.join('test', 'native_objc_test', '${testName}_config.yaml'))
           .readAsStringSync()) as YamlMap);
