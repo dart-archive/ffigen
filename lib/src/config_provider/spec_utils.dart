708a1efb4105552fe9ff3a0aaa4044112ad388ff
Liam Appelbe <liama@google.com>
2022-06-30 14:42:07 -0700

    More debugging

--- lib/src/config_provider/spec_utils.dart
+++ lib/src/config_provider/spec_utils.dart
@@ -465,7 +465,6 @@ String llvmPathExtractor(dynamic value) {
   // Extract path from default locations.
   try {
     final res = findDylibAtDefaultLocations();
-    print("Using clang: $res");
     return res;
   } catch (e) {
     _logger.severe(
