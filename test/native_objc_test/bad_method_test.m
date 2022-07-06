09dd4ba8a98dd58dcb853807c9b2641845f1d64e
Liam Appelbe <liama@google.com>
2022-07-06 09:27:00 -0700

    Bit field test

--- test/native_objc_test/bad_method_test.m
+++ test/native_objc_test/bad_method_test.m
@@ -6,6 +6,11 @@
 
 struct IncompleteStruct;
 
+struct BitField {
+  int x:3;
+  int y:12;
+};
+
 @interface BadMethodTestObject : NSObject {
 }
 
@@ -13,6 +18,11 @@ struct IncompleteStruct;
 - (struct IncompleteStruct*)incompletePointerReturn;  // Not skipped.
 - (int64_t)incompleteParam:(struct IncompleteStruct)x;  // Skipped.
 - (int64_t)incompletePointerParam:(struct IncompleteStruct*)x;  // Not skipped.
+
+- (struct BitField)bitFieldReturn;  // Skipped.
+- (struct BitField*)bitFieldPointerReturn;  // Not skipped.
+- (int64_t)bitFieldParam:(struct BitField)x;  // Skipped.
+- (int64_t)bitFieldPointerParam:(struct BitField*)x;  // Not skipped.
 
 @end
 
@@ -23,6 +33,14 @@ struct IncompleteStruct;
 }
 
 - (int64_t)incompletePointerParam:(struct IncompleteStruct*)x {
+  return (int64_t)x;
+}
+
+- (struct BitField*)bitFieldPointerReturn {
+  return (struct BitField*)5678;
+}
+
+- (int64_t)bitFieldPointerParam:(struct BitField*)x {
   return (int64_t)x;
 }
 
