c811efdd0d2e1332e22aacfe42b8cab46778ad63
Liam Appelbe <liama@google.com>
2022-07-07 08:38:48 -0700

    Check that property types aren't incomplete (#415)
    * Skip methods that have incomplete types
    
    Also change how method return types are parsed
    
    * Fix overly generic method return types
    
    * Fix tests
    
    * Bit field test
    
    * Fix the bug
    
    * Fix warning wording

--- test/native_objc_test/bad_method_test.m
+++ test/native_objc_test/bad_method_test.m
@@ -6,6 +6,11 @@
 
 struct IncompleteStruct;
 
+struct BitField {
+  int x:3;
+  int y:12;
+};
+
 @interface BadMethodTestObject : NSObject {
 }
 
@@ -13,6 +18,13 @@ struct IncompleteStruct;
 - (struct IncompleteStruct*)incompletePointerReturn;  // Not skipped.
 - (int64_t)incompleteParam:(struct IncompleteStruct)x;  // Skipped.
 - (int64_t)incompletePointerParam:(struct IncompleteStruct*)x;  // Not skipped.
+
+- (struct BitField)bitFieldReturn;  // Skipped.
+- (struct BitField*)bitFieldPointerReturn;  // Not skipped.
+- (int64_t)bitFieldParam:(struct BitField)x;  // Skipped.
+- (int64_t)bitFieldPointerParam:(struct BitField*)x;  // Not skipped.
+
+@property struct BitField bitFieldProperty;  // Skipped.
 
 @end
 
@@ -23,6 +35,14 @@ struct IncompleteStruct;
 }
 
 - (int64_t)incompletePointerParam:(struct IncompleteStruct*)x {
+  return (int64_t)x;
+}
+
+- (struct BitField*)bitFieldPointerReturn {
+  return (struct BitField*)5678;
+}
+
+- (int64_t)bitFieldPointerParam:(struct BitField*)x {
   return (int64_t)x;
 }
 
