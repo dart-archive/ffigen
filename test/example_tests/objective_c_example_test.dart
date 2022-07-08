b741926fe5ff680d34036c623bd818efdb4cc781
Liam Appelbe <liama@google.com>
2022-07-08 11:39:50 -0700

    Fix more analysis errors

--- test/example_tests/objective_c_example_test.dart
+++ test/example_tests/objective_c_example_test.dart
@@ -4,14 +4,16 @@
 
 // Objective C support is only available on mac.
 @TestOn('mac-os')
-import '../test_utils.dart';
 
 import 'dart:io';
+
 import 'package:ffigen/src/config_provider/config.dart';
 import 'package:ffigen/src/header_parser.dart';
 import 'package:logging/logging.dart';
 import 'package:test/test.dart';
 import 'package:yaml/yaml.dart';
+
+import '../test_utils.dart';
 
 void main() {
   group('objective_c_example_test', () {
